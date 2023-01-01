24fa629b834b4ae6330618e823e5e225273585be
Yasin Mohammadzade <103330832+YasinMhmmdzd@users.noreply.github.com>
2023-01-01 22:45:15 +0330

    add python files to project

---
+++ client.py
@@ -0,0 +1,21 @@
+from socket import *
+from colorama import Fore
+connection = socket(AF_INET , SOCK_STREAM)
+ip = input("Input your ip : ")
+port = input("input your port : ")
+user_name = input("Input Your username : ").encode()
+connection.connect((ip , int(port)))
+print(Fore.GREEN + 'connected')
+s_name = connection.recv(1024)
+connection.send(user_name)
+print(Fore.BLUE + str(s_name) + Fore.GREEN + " online")
+print("")
+while True:
+    print("")
+    data = connection.recv(1024)
+    data = str(data)
+    print(Fore.YELLOW + data)
+    pm = input(Fore.BLUE + ">>>").encode()
+    connection.send(pm)
+
+connection.close()
---
+++ server.py
@@ -0,0 +1,24 @@
+from socket import *
+from colorama import Fore
+server = socket(AF_INET , SOCK_STREAM)
+ip = input("Input your ip : ")
+port = input("input your port : ")
+user_name = input("Input your username : ").encode()
+server.bind((ip , int(port)))
+server.listen(1)
+print(Fore.GREEN + 'server running on port 1010')
+client , addr = server.accept()
+print(Fore.BLUE + "connected to " + str(addr))
+client.send(user_name)
+c_name = client.recv(1024)
+print(Fore.YELLOW + str(c_name) + Fore.GREEN + " online")
+print("")
+while True:
+    print("")
+    pm = input(Fore.BLUE + ">>>").encode()
+    client.send(pm)
+    rec_msg = client.recv(1024)
+    rec_msg = str(rec_msg)
+    print(Fore.YELLOW + rec_msg)
+
+client.close()
